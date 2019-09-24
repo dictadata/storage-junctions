1e74385d34a176b775373be82a60da6ea2106fa6
Drew Letcher <drew@drewletcher.net>
2019-09-24 08:05:00 -0500

    add tests store, recall and retieve

--- test/scripts/testelasticsearch.js
+++ test/scripts/testelasticsearch.js
@@ -3,11 +3,33 @@
  */
 "use strict";
 
+const store = require('./_store');
+const recall = require('./_recall');
+const retrieve = require('./_retrieve');
 const transfer = require('./_transfer');
 
 console.log("=== Tests: elasticsearch");
 
 async function tests() {
+
+  console.log("=== elasticsearch store");
+  await store({
+    src_smt: "elasticsearch|http://localhost:9200|test_input|=Foo",
+    construct: {}
+  });
+
+  console.log("=== elasticsearch recall");
+  await recall({
+    src_smt: "elasticsearch|http://localhost:9200|test_input|=Foo",
+    key: 1
+  });
+
+  console.log("=== elasticsearch retrieve");
+  await retrieve({
+    src_smt: "elasticsearch|http://localhost:9200|test_input|*",
+    pattern: { filter: {} }
+  });
+
 
   console.log("=== csv => elasticsearch");
   await transfer({
--- test/scripts/testmysql.js
+++ test/scripts/testmysql.js
@@ -1,28 +1,43 @@
 "use strict";
 
+const store = require('./_store');
 const recall = require('./_recall');
+const retrieve = require('./_retrieve');
 const transfer = require('./_transfer');
 
 console.log("=== Test: mysql");
 
 async function tests() {
 
-  console.log("=== test recall");
+  console.log("=== mysql store");
+  await store({
+    src_smt: "mysql|host=localhost;user=dicta;password=dicta;database=storage_node|test_table|=Foo",
+    construct: {}
+  });
+
+  console.log("=== mysql recall");
   await recall({
-    src_smt: "mysql|host=localhost;user=dicta;password=dicta;database=storage_node|test_table|*",
+    src_smt: "mysql|host=localhost;user=dicta;password=dicta;database=storage_node|test_table|=Foo",
     key: 1
   });
 
-  console.log("=== test reader");
+  console.log("=== mysql retrieve");
+  await retrieve({
+    src_smt: "mysql|host=localhost;user=dicta;password=dicta;database=storage_node|test_table|*",
+    pattern: { filter: {} }
+  });
+
+
+  console.log("=== mysql reader");
   await transfer({
     src_smt: "mysql|host=localhost;user=dicta;password=dicta;database=storage_node|test_table|*",
     dst_smt: "csv|./test/output/|mysql_output.csv|*"
   });
 
-  console.log("=== test writer");
+  console.log("=== mysql writer");
   await transfer({
     src_smt: "csv|./test/data/|testfile.csv|*",
-    dst_smt: "mysql|host=localhost;user=dicta;password=dicta;database=storage_node|test_writer|=Foo"
+    dst_smt: "mysql|host=localhost;user=dicta;password=dicta;database=storage_node|test_writer|*"
   });
 }
 
