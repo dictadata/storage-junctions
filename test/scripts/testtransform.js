6c86e00e8ed7b082fbb72b322937a3810ab0a2d3
Drew Letcher <drew@drewletcher.net>
2019-09-27 16:46:45 -0500

    transforms: inject, match, drop, mapping

--- test/scripts/testtransform.js
+++ test/scripts/testtransform.js
@@ -13,8 +13,22 @@ async function tests() {
     src_smt: "json|./test/data/|testfile.json|*",
     dst_smt: "json|./test/output/|transform_output.json|*",
     transforms: {
-      template: {},
-      transforms: {
+      inject: {
+        "newfield": "my new field"
+      },
+      match: {
+        "completed": {
+          op: 'eq',
+          value: false
+        }
+      },
+      drop: {
+        "id": {
+          "op": 'eq',
+          value: 5678
+        }
+      },
+      mapping: {
         "id": "id",
         "content": "content",
         "completed": "completed",
