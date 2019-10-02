50b447e1cd9c6c3acf008e7c8dae334625008cb7
Drew Letcher <drew@drewletcher.net>
2019-10-02 07:34:12 -0500

    elasticsearch refresh after store

--- test/scripts/_store.js
+++ test/scripts/_store.js
@@ -9,11 +9,13 @@ const logger = require('../../lib/logger');
 module.exports = async function (options) {
 
   logger.info(">>> create junction");
+  logger.verbose(options.src_smt);
+  logger.verbose(JSON.stringify(options.options));
   var junction = storage.activate(options.src_smt);
 
   try {
     let results = await junction.store(options.construct, options.options);
-    logger.info(results);
+    logger.verbose(JSON.stringify(results));
 
     logger.info(">>> completed");
     return results.key ? results.key : null;
