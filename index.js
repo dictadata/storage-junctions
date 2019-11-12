fdcac22fd5c182c51c3255f10cd7a068468c3a9a
Drew Letcher <drew@drewletcher.net>
2019-11-12 11:43:34 -0600

    add support for AWS Redshift

--- index.js
+++ index.js
@@ -16,38 +16,42 @@ exports.StorageError = require("./lib/storage_error");
 
 // standard junctions
 var CsvJunction = require("./lib/csv");
-cortex.use('csv',CsvJunction);
+cortex.use('csv', CsvJunction);
 exports.CsvJunction = CsvJunction;
 
 var ElasticsearchJunction = require("./lib/elasticsearch");
-cortex.use('elasticsearch',ElasticsearchJunction);
+cortex.use('elasticsearch', ElasticsearchJunction);
 exports.ElasticsearchJunction = ElasticsearchJunction;
 
 var JsonJunction = require("./lib/json");
-cortex.use('json',JsonJunction);   // defaults to json array
-cortex.use('jsons',JsonJunction);  // json stream
-cortex.use('jsonl',JsonJunction);  // json line
-cortex.use('jsona',JsonJunction);  // json array
-cortex.use('jsono',JsonJunction);  // json object
+cortex.use('json', JsonJunction);   // defaults to json array
+cortex.use('jsons', JsonJunction);  // json stream
+cortex.use('jsonl', JsonJunction);  // json line
+cortex.use('jsona', JsonJunction);  // json array
+cortex.use('jsono', JsonJunction);  // json object
 exports.JsonJunction = JsonJunction;
 
 var MongoDBJunction = require("./lib/mongodb");
-cortex.use('mongodb',MongoDBJunction);
+cortex.use('mongodb', MongoDBJunction);
 exports.MongoDBJunction = MongoDBJunction;
 
 var MSSQLJunction = require("./lib/mssql");
-cortex.use('mssql',MSSQLJunction);
+cortex.use('mssql', MSSQLJunction);
 exports.MSSQLJunction = MSSQLJunction;
 
 var MySQLJunction = require("./lib/mysql");
-cortex.use('mysql',MySQLJunction);
+cortex.use('mysql', MySQLJunction);
 exports.MySQLJunction = MySQLJunction;
 
+var RedshiftJunction = require("./lib/redshift");
+cortex.use('redshift', RedshiftJunction);
+exports.RedshiftJunction = RedshiftJunction;
+
 var PostgreSQLJunction = require("./lib/postgresql");
-cortex.use('postgresql',PostgreSQLJunction);
-cortex.use('postgres',PostgreSQLJunction);
+cortex.use('postgresql', PostgreSQLJunction);
+cortex.use('postgres', PostgreSQLJunction);
 exports.PostgreSQLJunction = PostgreSQLJunction;
 
 var RestJunction = require("./lib/rest");
-cortex.use('rest',RestJunction);
+cortex.use('rest', RestJunction);
 exports.RestJunction = RestJunction;
