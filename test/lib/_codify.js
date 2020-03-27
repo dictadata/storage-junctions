39b018e767a68261af3a81b0e39f39397567a98a
Drew Letcher <drew@drewletcher.net>
2020-03-26 19:22:51 -0500

    include optional transforms in codify test

--- test/lib/_codify.js
+++ test/lib/_codify.js
@@ -13,13 +13,14 @@ const fs = require('fs');
 const pipeline = util.promisify(stream.pipeline);
 
 module.exports = exports = async function (options) {
-
   logger.info(">>> create junction");
+  if (!options.transforms) options.transforms = {};
+
   var j1;
   try {
     j1 = await storage.activate(options.source.smt, options.source.options);
 
-    // *** the normal way is to ask the junction to do it
+    // *** get encoding for junction's schema
     logger.info(">>> getEncoding");
     let encoding1 = await j1.getEncoding();
 
@@ -27,14 +28,20 @@ module.exports = exports = async function (options) {
     logger.info(">>> save encoding to " + options.outputFile1);
     fs.writeFileSync(options.outputFile1, JSON.stringify(encoding1,null,"  "), "utf8");
 
-    // *** stream some data to the codifier
-    logger.info(">>> create streams");
-    var reader = j1.getReadStream({max_read: (options.source.options && options.source.options.max_read) || 100 });
-    var codify = j1.getCodifyWriter(options.codify ||{});
+    // *** use CodifyWriter to determine encoding including optional transforms
+    logger.info(">>> build pipeline");
+    let pipes = [];
+    pipes.push(j1.getReadStream({max_read: (options.source.options && options.source.options.max_read) || 100 }));
+    for (let [tfType,tfOptions] of Object.entries(options.transforms))
+      pipes.push(j1.getTransform(tfType, tfOptions));
+    let codify = j1.getCodifyWriter(options.codify ||{});
+    pipes.push(codify);
 
-    logger.info(">>> start pipe");
-    await pipeline(reader, codify);
+    // run the pipeline and get the resulting encoding
+    logger.info(">>> start pipeline");
+    await pipeline(pipes);
 
+    // save the codify results
     let encoding2 = await codify.getEncoding();
 
     logger.debug(JSON.stringify(encoding2, null, "  "));
