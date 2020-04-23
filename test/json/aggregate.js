619fc005e109cccb78375dc4984348c1e287be77
Drew Letcher <drew@drewletcher.net>
2020-04-23 12:07:05 -0500

    refactor consolidate to aggregate

---
+++ test/json/aggregate.js
@@ -0,0 +1,85 @@
+/**
+ * test/json
+ */
+"use strict";
+
+const transfer = require('../lib/_transfer');
+const logger = require('../../lib/logger');
+
+logger.info("=== Tests: retreive");
+
+async function tests() {
+/*
+  logger.info("=== json aggregate");
+  await transfer({
+    origin: {
+      smt: "json|./test/data/|foofile.json|*"
+    },
+    "transforms": {
+      filter: {
+        match: {
+          "Bar": "row",
+          "Baz": { "gte": 0, "lte": 1000 }
+        }
+      },
+      aggregate: {
+        "baz_sum": { "sum": "Baz" },
+        "fobe_max": { "max": "Fobe" }
+      }
+    },
+    terminal: {
+      "smt": 'json|./test/output/|json_aggregate_1.json|*'
+    }
+  });
+
+  logger.info("=== json groupby with summary");
+  await transfer({
+    origin: {
+      smt: "json|./test/data/|foofile.json|*",
+    },
+    "transforms": {
+      filter: {
+        match: {
+          "Bar": "row",
+          "Baz": { "gte": 0, "lte": 1000 }
+        }
+      },
+      aggregate: {
+        "sum": { "sum": "Baz" },
+        "avg": { "avg": "Baz" },
+        "min": { "min": "Baz" },
+        "max": { "max": "Baz" },
+        "count": { "count": "Baz" }
+      }
+    },
+    terminal: {
+      "smt": 'json|./test/output/|json_aggregate_2.json|*'
+    }
+  });
+*/
+  logger.info("=== json aggregate w/ groupby");
+  await transfer({
+    origin: {
+      smt: "json|./test/data/|foofile.json|*"
+    },
+    transforms: {
+      filter: {
+        match: {
+          "Baz": { "gte": 0, "lte": 1000 }
+        }
+      },
+      aggregate: {
+        "Foo": {
+          "baz_sum": { "sum": "Baz" },
+          "count": { "count": "Baz" }
+        }
+      }
+    },
+    terminal: {
+      "smt": 'json|./test/output/|json_aggregate_3.json|*'
+    }
+  });
+
+}
+
+tests();
