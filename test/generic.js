7bc8ce9bb1914a134d17819d4b97cba1ade7aabe
drewlab <drew@drewletcher.net>
2020-10-10 12:10:23 -0500

    add generice junction for model *

---
+++ test/generic.js
@@ -0,0 +1,44 @@
+/**
+ * test/junctions
+ */
+"use strict";
+
+const storage = require("../lib/index");
+const logger = require('../lib/logger');
+
+logger.info("=== Tests: Generic * Junction");
+
+async function test_1() {
+
+  var junction;
+  try {
+    logger.info(">>> create junction");
+    junction = await storage.activate({
+      smt: {
+        model: "*",
+        locus: "file:./test/data/",
+        schema: "*",
+        key: "*"
+      }
+    });
+
+    logger.info(">>> create filesystem");
+    var stfs = junction.getFileSystem();
+
+
+    logger.info(">>> relax junction");
+    if (junction) await junction.relax();
+
+  }
+  catch (err) {
+    logger.error(err.message);
+  }
+  finally {
+    if (junction) await junction.relax();
+  }
+}
+
+(async () => {
+  await test_1();
+  logger.info(">>> completed");
+})();
