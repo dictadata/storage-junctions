a45952bc8434c4c2a2b68ffbd00f59378e12cc7c
drewlab <drew@dictadata.org>
2021-02-17 12:07:17 -0600

    new test runner

---
+++ testlaunch.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const { spawn } = require('child_process');
+
+let category = process.argv.length > 2 ? process.argv[2] : "";
+
+(async () => {
+  let l = fs.readFileSync("./.vscode/launch.json", "utf-8");
+  let lj = l.replace(/\/\/.*/g, "");
+  var launch = JSON.parse(lj);
+
+  for (let config of launch.configurations) {
+    if (!category || config.name.indexOf(category) === 0) {
+      console.log(config.name);
+      if (config.type === "pwa-node" && config.request === "launch") {
+        let script = config.program.replace("${workspaceFolder}", ".");
+        let args = [script];
+        if (config.args)
+          args.push(config.args);
+        let exitcode = await runTest(args);
+        if (exitcode !== 0)
+          break;
+      }
+    }
+  }
+})();
+
+async function runTest(args) {
+  return new Promise((resolve, reject) => {
+    const program = spawn('node', args);
+
+    program.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    program.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    program.on('close', (code) => {
+      console.log(`child process close with code ${code}`);
+      resolve(code);
+    });
+
+    program.on('disconnect', () => {
+      console.log(`child process disconnect`);
+      resolve(code);
+    });
+
+    program.on('error', (error) => {
+      console.log(`child process error ${error.message}`);
+      resolve(code);
+    });
+/*
+    program.on('exit', (code) => {
+      console.log(`child process exit with code ${code}`);
+      resolve(code);
+    });
+*/
+  });
+}
