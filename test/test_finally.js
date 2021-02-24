86b2fdb58d23dad08a421fd08bd3c2b0c3a7cb3b
drewlab <drew@dictadata.org>
2021-02-24 10:05:40 -0600

    test behavoir of try-catch-finally

---
+++ test/test_finally.js
@@ -0,0 +1,31 @@
+function thecode() {
+
+  try {
+      console.log("trying outer");
+    try {
+      console.log("trying inner");
+      throw new Error('oops');
+      return "gotcha";
+    } catch (ex) {
+      console.error('inner', ex.message);
+      return "no throw";
+      throw ex;
+    } finally {
+      console.log('finally');
+    }
+  } catch (ex) {
+    console.error('outer', ex.message);
+  }
+
+  return "normal";
+}
+
+(() => {
+  let results = thecode();
+  console.log(results);
+})();
+
+// Output:
+// "inner" "oops"
+// "finally"
+// "outer" "oops"
