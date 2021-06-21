995c3d48efb5100891345ca48951e328ad7e24cb
drewlab <drew@dictadata.org>
2021-06-20 20:16:31 -0500

    census etl tests

--- test/shapefile/encoding.js
+++ test/shapefile/encoding.js
@@ -40,14 +40,25 @@ async function test2() {
 
 async function test3() {
 
-  logger.info("=== rest getEncoding tl_2020_us_states");
+  logger.info("=== rest getEncoding tl_2020_us_state");
   if (await getEncoding({
     origin: {
       smt: "shp|zip:/var/data/www2.census.gov/geo/tiger/TIGER2020/STATE/tl_2020_us_state.zip|tl_2020_us_state|*",
       options: {}
     },
     terminal: {
-      output: './test/data/output/shapefile/tl_2020_us_states_encoding.json'
+      output: './test/data/output/shapefile/tl_2020_us_state_encoding.json'
+    }
+  }, false)) return 1;
+
+  logger.info("=== rest getEncoding tl_2020_us_county");
+  if (await getEncoding({
+    origin: {
+      smt: "shp|zip:/var/data/www2.census.gov/geo/tiger/TIGER2020/COUNTY/tl_2020_us_county.zip|tl_2020_us_state|*",
+      options: {}
+    },
+    terminal: {
+      output: './test/data/output/shapefile/tl_2020_us_county_encoding.json'
     }
   }, false)) return 1;
 
