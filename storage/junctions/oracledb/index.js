6c9ebec0a2a33583cd52d3666726535f75af64ad
drewlab <drew@dictadata.org>
2021-06-28 08:41:23 -0500

    moved to oracledb-junction project

--- storage/junctions/oracledb/index.js
+++
@@ -1,9 +0,0 @@
-// junctions/oracledb
-
-const OracleDBJunction = require("./oracledb-junction");
-
-module.exports = exports = OracleDBJunction;
-exports.OracleDBReader = require("./oracledb-reader");
-exports.OracleDBWriter = require("./oracledb-writer");
-exports.OracleDBEncoder = require("./oracledb-encoder");
-exports.OracleDBSQLEncoder = require("./oracledb-sql-encoder");
