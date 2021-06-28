4cb89c6c5bbc162bd61a7190cbc6d4ee320200e5
drewlab <drew@dictadata.org>
2021-06-28 08:39:32 -0500

    moved to mongodb-junction project

--- storage/junctions/mongodb/index.js
+++
@@ -1,8 +0,0 @@
-// junctions/mongodb
-
-const MongoDBJunction = require("./mongodb-junction");
-
-module.exports = exports = MongoDBJunction;
-exports.MongoDBReader = require("./mongodb-reader");
-exports.MongoDBWriter = require("./mongodb-writer");
-//exports.MongoDBEncoder = require("./mongodb-encoder");
