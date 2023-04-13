1191f8eba590dfb8071568983b538ed8fb3f89db
drewlab <drew@dictadata.org>
2023-04-13 12:19:11 -0500

    support ssl (tls) connection options

--- test/mysql/createSchema.js
+++ test/mysql/createSchema.js
@@ -5,7 +5,9 @@
 
 const createSchema = require('../lib/_createSchema');
 const dull = require("../lib/_dull");
-const { logger } = require('../../storage/utils');
+const { logger, typeOf } = require('../../storage/utils');
+const fs = require('node:fs');
+const homedir = require('os').homedir();
 
 logger.info("=== Test: mysql createSchema");
 
@@ -52,6 +54,48 @@ async function test_lg() {
 
 }
 
+async function test_origin(schema, encoding) {
+
+  let ca_file = fs.readFileSync(homedir + "/.dictadata/ec2_mysql-ca.pem");
+  console.log(typeOf(ca_file));
+
+  logger.info("=== createSchema " + schema);
+  if (await createSchema({
+    origin: {
+      smt: "mysql|host=data-origin.dictadata.org;database=storage_node|" + schema + "|*",
+      options: {
+        auth: {
+          username: "dicta",
+          password: "data"
+        },
+        ssl: {
+          ca: ca_file,
+          rejectUnauthorized: true
+        },
+        encoding: "./data/input/" + encoding + ".encoding.json"
+      }
+    }
+  })) return 1;
+
+  logger.info("=== dull (truncate) " + schema);
+  if (await dull({
+    origin: {
+      smt: "mysql|host=data-origin.dictadata.org;database=storage_node|" + schema + "|*",
+      options: {
+        auth: {
+          username: "dicta",
+          password: "data"
+        },
+        ssl: {
+          ca: ca_file,
+          rejectUnauthorized: true
+        }
+      }
+    }
+  })) return 1;
+
+}
+
 (async () => {
   if (await test("foo_schema", "foo_schema")) return 1;
   if (await test("foo_schema_x", "foo_schema")) return 1;    // for dullSchema.js
@@ -59,4 +103,6 @@ async function test_lg() {
   if (await test("foo_schema_02", "foo_schema_02")) return 1;
   if (await test("foo_schema_two", "foo_schema_two")) return 1;
   if (await test_lg()) return 1;
+
+  if (await test_origin("foo_schema", "foo_schema")) return 1;
 })();
