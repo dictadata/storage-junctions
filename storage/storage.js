4a21aa3a5269d6f6e7efab95259e47bf4f8a335e
drewlab <drew@dictadata.org>
2024-02-28 21:13:40 -0600

    refactor Storage activate method

--- storage/storage.js
+++ storage/storage.js
@@ -17,44 +17,43 @@ const auth = require("./authentication");
 
 const { SMT } = require("./types");
 
-class Storage {
+/**
+ * Create and activate a StorageJunction given an SMT.
+ * Will do Engrams engram and auth lookups.
+ *
+ * @param {*} smt an SMT name, SMT string or SMT object
+ * @param {*} options options to pass to the storage-junction
+ * @returns
+ */
+async function activate(smt, options) {
+  let _smt = {};
+  if (!options)
+    options = {};
 
-  /**
-   * Create and activate a StorageJunction given an SMT.
-   * Will do Engrams engram and auth lookups.
-   *
-   * @param {*} smt an SMT name, SMT string or SMT object
-   * @param {*} options options to pass to the storage-junction
-   * @returns
-   */
-  static async activate(smt, options) {
-    let _smt = {};
-    if (!options)
-      options = {};
+  // SMT string or object
+  _smt = new SMT(smt);
 
-    // SMT string or object
-    _smt = new SMT(smt);
-
-    // check for auth options
-    if (!options.auth && auth.has(_smt.locus)) {
-      let credentials = auth.recall(_smt.locus);
-      options = Object.assign(options, credentials);
-    }
-
-    // create the junction
-    let junction;
-    if (_smt.model === "$" && options.junction)
-      junction = options.junction;
-    else
-      junction = Junctions.activate(_smt, options);
-    return junction;
+  // check for auth options
+  if (!options.auth && auth.has(_smt.locus)) {
+    let credentials = auth.recall(_smt.locus);
+    options = Object.assign(options, credentials);
   }
 
+  // create the junction
+  let junction;
+  if (_smt.model === "$" && options.junction)
+    junction = options.junction;
+  else
+    junction = Junctions.activate(_smt, options);
+  return junction;
 }
+
+class Storage {}
 
 Storage.Junctions = Junctions;
 Storage.Transforms = Transforms;
 Storage.FileSystems = FileSystems;
 Storage.auth = auth;
+Storage.activate = activate;
 
 module.exports = exports = Storage;
